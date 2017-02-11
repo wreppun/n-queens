38853790c06d2607631d4ef9c9c4105313408b5a
Will Reppun <(none)>
2017-02-11 11:03:12 -1000

    :wrench: extract common test logic

--- src/test/java/com/compilerworks/nqueens/WikipediaATest.java
+++ src/test/java/com/compilerworks/nqueens/WikipediaATest.java
@@ -2,16 +2,11 @@ package com.compilerworks.nqueens;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class WikipediaATest {
-	private static List<Integer> expectedEight = Arrays.asList(4, 6, 8, 2, 7, 1, 3, 5);
-	private static List<Integer> expectedNine = Arrays.asList(4, 6, 8, 2, 7, 1, 3, 5, 9);
-	private static List<Integer> expectedTen = Arrays.asList(2, 4, 6, 8, 10, 1, 3, 5, 7, 9);
-
 	private Solver fixture = new WikipediaA();
 
 	@Test
@@ -20,7 +15,7 @@ public class WikipediaATest {
 
 		assertEquals(1, oneIndexedSolutions.size());
 
-		assertMatch(expectedEight, oneIndexedSolutions.get(0));
+		TestUtils.assertMatch(TestUtils.expectedEight, oneIndexedSolutions.get(0));
 	}
 
 	@Test
@@ -29,7 +24,7 @@ public class WikipediaATest {
 
 		assertEquals(1, oneIndexedSolutions.size());
 
-		assertMatch(expectedNine, oneIndexedSolutions.get(0));
+		TestUtils.assertMatch(TestUtils.expectedNine, oneIndexedSolutions.get(0));
 	}
 
 	@Test
@@ -38,14 +33,6 @@ public class WikipediaATest {
 
 		assertEquals(1, oneIndexedSolutions.size());
 
-		assertMatch(expectedTen, oneIndexedSolutions.get(0));
-	}
-
-	private static void assertMatch(List<Integer> expected, List<Integer> solution) {
-		assertEquals(expected.size(), solution.size());
-
-		for (int i = 0; i < solution.size(); i++) {
-			assertEquals(expected.get(i), solution.get(i));
-		}
+		TestUtils.assertMatch(TestUtils.expectedTen, oneIndexedSolutions.get(0));
 	}
 }
